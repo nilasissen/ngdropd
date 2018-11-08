4b75f945a7acf482e5197effa8fefcd6d79218e5
Nilasis sen <sen.nilasis010@gmail.com>
2018-11-08 09:30:27 +0530

    show custome popover done design part remainging

--- js/qiSpa.js
+++ js/qiSpa.js
@@ -1,37 +1,44 @@
-angular.module('myApp',[])
-.controller('dropDownController',['$scope','$rootScope',function($scope,$rootScope){
+angular.module('myApp', [])
+  .controller('dropDownController', ['$scope', '$rootScope', '$http', function ($scope, $rootScope,$http) {
     var self = this;
-    self.values = [{
-        "driver":{
-            "title":"DRIVER",
-            "name":"Li Nanxing",
-            "phone":"+6594457422"
-        },
-        "car":{
-            "title":"VEHICLE",
-            "vehicle_number" :"SGD1234F",
-            "vehicle_class":"Lamborghini Aventador"
+    // demo server json data
+    $http.get('./js/driver.json')
+       .then((res)=>{
+          self.values = res.data;                
+        });
+    //header msg
+    self.greetings = 'Drop Down';
+  }])
+  //drop down simple directive
+  .directive('dropdown', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        list: '='
+      },
+      template: '<div class="dropdown">\
+      <button class="dropbtn">Select Driver / Paird Vehicle</button>\
+      <div class="dropdown-content">\
+      <a data-ng-repeat="i in data track by $index" data-ng-mouseover="showModal(i)" >\
+      {{i.driver.name}} / {{i.car.vehicle_number}}</a>\
+      <div data-ng-class="{alpha: alpha}" class="popover__content">\
+      <ul class="car-driver-ul"><li data-ng-if="selected.driver.id"><p class="popover__message">{{selected.driver.title}}</p>\
+      <img class="prof-Image" data-ng-src="{{selected.driver.driver_image}}"><span class="popover__message">{{selected.driver.name}}</span><br><span class="popover__message">{{selected.driver.phone}}</span></li>\
+      <li data-ng-if="selected.car.id"><p class="popover__message">{{selected.car.title}}</p>\
+      <img class="prof-Image" data-ng-src="{{selected.car.vehicle_image}}"><span class="popover__message">{{selected.car.vehicle_number}}</span><br><span class="popover__message">{{selected.car.vehicle_class}}</span></li>\
+      </div>\
+      </div></div>',
+      controller: function ($scope) {
+        $scope.data = $scope.list;
+        $scope.selected = '';
+        $scope.alpha = false;
+        // on hover getting the data from html
+        $scope.showModal = function (hoverData) {
+          $scope.alpha = true
+          $scope.selected = hoverData;
+          var myEl = angular.element(document.querySelector('#popC'));
+          myEl.addClass('showPop');
         }
-        },
-        {
-            "driver":{
-                "title":"DRIVER",
-                "name":"Li Yinzhu",
-                "phone":"+6594457422"
-            },
-            "car" :{
-    
-            }
-        },{
-          "driver" :{
-    
-          },
-          "car":{
-            "title":"VEHICLE",
-            "vehicle_number" :"LKK1200F",
-            "vehicle_class":"Jaguar"
-        }
-        }];
-    console.log('Loading Controller...');
-    self.greetings = 'Hello world';
-    }])
+      }
+    }
+  });
