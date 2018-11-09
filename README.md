49473304dc27326e05ab2cb9d25f8b46334ba6a7
Nilasis sen <sen.nilasis010@gmail.com>
2018-11-09 20:21:01 +0530

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ngdropd
+### Installation
+Download unzip using command line -
+```sh
+$ cd ngdropd
+$ python -m SimpleHTTPServer
+```
+
+Verify the deployment by navigating to your server address in your preferred browser.
+
+```sh
+127.0.0.1:4200
+```
--- js/qiSpa.js
+++ js/qiSpa.js
@@ -4,7 +4,9 @@ angular.module('myApp', [])
     // demo server json data where i have kept the driver and car json
     $http.get('./js/driver.json')
        .then((res)=>{
+         if(res) {
           self.values = res.data;                
+         }
         });
     //header msg
     self.greetings = 'Drop Down';
@@ -21,9 +23,9 @@ angular.module('myApp', [])
         list: '='
       },
       template: '<div class="dropdown" >\
-      <button class="dropbtn">Select Driver / Paird Vehicle</button>\
+      <button class="dropbtn"> {{ selectedValue || "Select Driver / Paird Vehicle" }}</button>\
       <div class="dropdown-content">\
-      <a data-ng-repeat="i in data track by $index"   data-ng-mouseover="showModal(i)" >\
+      <a data-ng-repeat="i in data track by $index" data-ng-click="selectValue(i)"  data-ng-mouseover="showModal(i)" >\
       {{i.driver.name}} / {{i.car.vehicle_number}}</a>\
       </div></div><div data-ng-class="{alpha: alpha}" class="popover__content">\
       <ul class="car-driver-ul"><li data-ng-if="selected.driver.id"><p class="popover__message">{{selected.driver.title}}</p>\
@@ -31,21 +33,28 @@ angular.module('myApp', [])
       <li data-ng-if="selected.car.id"><p class="popover__message">{{selected.car.title}}</p>\
       <img class="prof-Image" data-ng-src="{{selected.car.vehicle_image}}"><span class="popover__message">{{selected.car.vehicle_number}}</span><br><span class="popover__message">{{selected.car.vehicle_class}}</span></li></ul>\
       </div>',
-      controller: function ($scope) {
+      controller:  ($scope) => {
         $scope.data = $scope.list;
         $scope.selected = '';
+        $scope.selectedValue = '';
         // this variable will be used to show hide the popover
         $scope.alpha = false;
         // receiving event and hiding the popover
-        $scope.$on('alphaFalse',(ev,v)=>{
+        $scope.$on('alphaFalse',(ev,v) => {
           $scope.alpha = false;
         })
         // on hover getting the data from html
-        $scope.showModal = function (hoverData) {
+        $scope.showModal = (hoverData) => {
           $scope.alpha = true
           $scope.selected = hoverData;
           var myEl = angular.element(document.querySelector('#popC'));
           myEl.addClass('showPop');
+        }
+        // to get selected value
+        $scope.selectValue = (v) => {
+          $scope.alpha = false;
+          $scope.selectedValue = v.driver.id ? v.driver.name: '-' + '/' + v.car.id ?  v.car.vehicle_number : '-';
+          
         }
       }
     }
